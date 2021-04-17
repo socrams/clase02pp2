f59f398147adbbe983a0906b98bec1cf48c66c60
unknown <socrams.88@gmail.com>
2021-04-16 22:03:18 -0300

    ej2

--- app/ejercicio2.php
+++ app/ejercicio2.php
@@ -1,6 +1,6 @@
 <?php
 $precio=rand(1,999);
-$X=$precio*21/100;
-echo $precio;
-echo " El precio + iva es = ".$X;
+$X=($precio*21/100)+$precio;
+echo "Precio sin IVA ".$precio;
+echo " El precio con IVA ".$X;
 ?>
