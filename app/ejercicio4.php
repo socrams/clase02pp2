50588c52954a28a076ed32b53f404446aca58ded
unknown <socrams.88@gmail.com>
2021-04-23 18:58:07 -0300

    ej4

--- app/ejercicio4.php
+++ app/ejercicio4.php
@@ -2,16 +2,18 @@
 $num1=rand(1,99);
 $num2=rand(1,99);
 $num3=rand(1,99);
-echo $num1.$num2.$num3;
+echo $num1;
+echo $num2;
+echo $num3;
 If($num1 > $num2){
 	If($num1 > $num3){
-	echo 'numero ' . $num1 . ' es mayor';
+		echo 'numero ' . $num1 . ' es mayor';
 	} 
 	else {
 		echo 'numero ' . $num3 . ' es mayor';
 	}
-	} 
-	else {
+} 
+else {
 	If($num2 > $num3){
 		echo 'numero ' . $num2 . ' es mayor';
 	} 
