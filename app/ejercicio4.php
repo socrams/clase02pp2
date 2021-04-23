b5ec48dde21395d4961c9a0715ef9d432df83b7b
unknown <socrams.88@gmail.com>
2021-04-23 19:24:49 -0300

    ej4

--- app/ejercicio4.php
+++ app/ejercicio4.php
@@ -2,23 +2,20 @@
 $num1=rand(1,99);
 $num2=rand(1,99);
 $num3=rand(1,99);
-echo $num1" ";
-echo $num2" ";
-echo $num3" ";
-$numMayor="nada";
-if($num1 >$num2 && $num1 > $num3)
+$numMayor="";
+if($num1>$num2 && $num1>$num3)
 {
-	$numMayor =$num1;	//		echo "	numero " . $num1 . "es mayor";
-} 
+	$numMayor =$num1;
+}
 else
 {
 	if ($num2>$num3)
 	{
-		$numMayor=$num2; 	//echo " numero ".$num2. " es mayor";
+		$numMayor=$num2;
 	}
 	else 
 	{
-		$numMayor=$num3; 	//echo "	numero " . $num3 . " es mayor";
+		$numMayor=$num3;
 	}
 }
 echo "el numero es ".$numMayor;
