5f5933f746f5097d78c3085cd581db88bd4d28f8
unknown <socrams.88@gmail.com>
2021-04-23 20:13:04 -0300

    ej5

--- app/ejercicio5.php
+++ app/ejercicio5.php
@@ -2,24 +2,24 @@
 $edad=rand(1,99);
 switch ($edad)
 {
-	case 1:echo "Es menor ".$edad;break;
-	case 2:echo "Es menor ".$edad;break;
-	case 3:echo "Es menor ".$edad;break;
-	case 4:echo "Es menor ".$edad;break;
-	case 5:echo "Es menor ".$edad;break;
-	case 6:echo "Es menor ".$edad;break;
-	case 7:echo "Es menor ".$edad;break;
-	case 8:echo "Es menor ".$edad;break;
-	case 9:echo "Es menor ".$edad;break;
-	case 10:echo "Es menor ".$edad;break;
-	case 11:echo "Es menor ".$edad;break;
-	case 12:echo "Es menor ".$edad;break;
-	case 13:echo "Es adolecente ".$edad;break;
-	case 14:echo "Es adolecente ".$edad;break;
-	case 15:echo "Es adolecente ".$edad;break;
-	case 16:echo "Es adolecente ".$edad;break;
-	case 17:echo "Es adolecente ".$edad;break;
-	case 18:echo "Es adolecente ".$edad;break;
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	case 12:
+		echo "Es menor".$edad;break;
+	case 13:
+	case 14:
+	case 15:
+	case 16:
+	case 18:echo " Es adolecente ".$edad;break;
 	default:
 		echo "Es mayor de edad ".$edad;
 }
---
+++ app/ejercicio6.php
@@ -0,0 +1,11 @@
+<?php
+$edad=rand(1,99);
+switch ($edad)
+{
+	case "Bariloche":echo "Es menor ".$edad;break;
+	case "Usuahia":echo "Es menor ".$edad;break;
+	case "Cataratas":echo "Es menor ".$edad;break;
+
+}
+
+?>
