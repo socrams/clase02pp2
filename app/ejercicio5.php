18eb19a65204e5510989eca27441bd0bca85565c
unknown <socrams.88@gmail.com>
2021-04-23 20:00:19 -0300

    ej5

--- app/ejercicio5.php
+++ app/ejercicio5.php
@@ -1,6 +1,6 @@
 <?php
 $edad=rand(1,99);
-switch ($edad);
+switch ($edad)
 {
 	case 1:echo "Es menor ".$edad;break;
 	case 2:echo "Es menor ".$edad;break;
@@ -21,7 +21,7 @@ switch ($edad);
 	case 17:echo "Es adolecente ".$edad;break;
 	case 18:echo "Es adolecente ".$edad;break;
 	default:
-		echo "Es mayor de edad "$edad;
+		echo "Es mayor de edad ".$edad;
 }
 
 ?>
